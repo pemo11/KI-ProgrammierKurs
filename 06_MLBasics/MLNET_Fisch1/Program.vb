6b5d770d2922f22e57c3389d4e606e1929a78a66
Peter Monadjemi <pm@activetraining.de>
2024-12-13 17:20:39 +0100

    Aktueller Stand 13/12/24 - Fish-Klassifizierung

--- 06_MLBasics/MLNET_Fisch1/Program.vb
+++ 06_MLBasics/MLNET_Fisch1/Program.vb
@@ -1,7 +1,26 @@
-' Fisch-Kategorisierung mit einem einfachen binären Klassifikator
+' Fisch-Klassifizierung mit einem einfachen binären Klassifikator
 ' Erstellt: 13/12/24
+
 Imports Microsoft.ML
 Imports Microsoft.ML.Data
+
+Imports System.IO
+
+Class FishData
+    <LoadColumn(0)>
+    Public Property Length As Single
+    <LoadColumn(1)>
+    Public Property Density As Single
+    <LoadColumn(2)>
+    Public Property FishType As Boolean
+End Class
+
+Class FishPrediction
+    <ColumnName("PredictedLabel")>
+    Public Property PredictedLabel As Boolean
+    Public Property Probability As Single
+    Public Property Score As Single
+End Class
 
 Module Program
 
@@ -10,7 +29,7 @@ Module Program
         Dim mlContext As New MLContext
 
         ' Trainingsdaten laden
-        Dim csvPath = Path.Join(Environment.CurrentDirectory, "Data", "fish-data.csv")
+        Dim csvPath = Path.Join(Environment.CurrentDirectory, "..", "data", "fish-data.csv")
         Dim fishData As IDataView = mlContext.Data.LoadFromTextFile(Of FishData)(
             csvPath, separatorChar:=","c, hasHeader:=True)
 
@@ -20,7 +39,8 @@ Module Program
         Dim testData As IDataView = dataSplit.TestSet
 
         ' Pipeline definieren
-        Dim Pipeline = mlContext.Transforms.Concatenate("Features", "Length", "Density") _
+        Dim Pipeline = mlContext.Transforms.CopyColumns("Label", "FishType") _
+         .Append(mlContext.Transforms.Concatenate("Features", "Length", "Density")) _
          .Append(mlContext.BinaryClassification.Trainers.SdcaLogisticRegression())
 
         ' Modell trainieren
@@ -38,14 +58,20 @@ Module Program
         Console.WriteLine($"F1-Score: {metrics.F1Score}")
 
         ' Modell speichern
-        Dim modelPath = Path.Join(Environment.CurrentDirectory, "Data", "FishClassifier.zip")
+        Dim modelPath = Path.Join(Environment.CurrentDirectory, "..", "Data", "FishClassifier.zip")
         mlContext.Model.Save(model, trainData.Schema, modelPath)
 
         Console.WriteLine("Modell gespeichert.")
 
+        ' Model wieder laden
+        Dim loadedModel As ITransformer
+        Using fs = new FileStream(modelPath, FileMode.Open, FileAccess.Read)
+            loadedModel = mlContext.Model.Load(fs, inputSchema := Nothing)
+            Console.WriteLine("Model wurde geladen.")
+        End Using
+ 
         ' Vorhersage treffen
-        Dim predictor = mlContext.Model.Load(modelPath, out var modelSchema)
-        Dim predictionEngine = mlContext.Model.CreatePredictionEngine(Of FishData, FishPrediction)(predictor)
+        Dim predictionEngine = mlContext.Model.CreatePredictionEngine(Of FishData, FishPrediction)(loadedModel)
 
         Dim fish1 = New FishData With {.Length = 25.4F, .Density = 0.98F}
         Dim fish2 = New FishData With {.Length = 26.3F, .Density = 0.98F}
@@ -53,5 +79,9 @@ Module Program
         Dim prediction1 = predictionEngine.Predict(fish1)
         Dim prediction2 = predictionEngine.Predict(fish2)
 
-        Console.WriteLine($"Fisch 1: {prediction1.Prediction}")
-        Console.WriteLine($"Fisch 2: {prediction2.Prediction}")
+        Console.WriteLine($"Fisch 1: {prediction1.PredictedLabel}")
+        Console.WriteLine($"Fisch 2: {prediction2.PredictedLabel}")
+
+    End Sub
+
+End Module
